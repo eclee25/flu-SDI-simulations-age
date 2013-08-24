9dd34bcf02b2eac0f09b0ec7c6bbd3f683d31026
Elizabeth Lee <eclee25@gmail.com>
2013-08-24 00:49:42 -0400

    perc fxn, fixed OR by episize, plt colors, cleaned
    age_perc_T.py
    - changed code to calculate OR using new perc_age_gen
    function in percolations.py
    - OR by episizes charts were updated to include all
    results and epidemics only with careful sorting of
    dict keys
    - plot colors for each T value in OR by episize are
    now a rainbow using colormap
    - added additional comments and organized code to
    improve readability

--- age_perc_T.py
+++ age_perc_T.py
@@ -5,12 +5,11 @@
 ###Author: Elizabeth Lee
 ###Date: 8/7/13
 ###Function:
-##### 1) odds ratio by T
-##### 2) odds ratio by vax efficacy
+##### explore relationship between OR and T in age-structured network
 
 ###Import data: urban_edges_Sarah.csv, urban_ages_Sarah.csv
 
-###Command Line: python age_perc.py
+###Command Line: python age_perc_T.py
 ##############################################
 
 ### notes ###
@@ -30,35 +29,39 @@ import numpy as np
 from time import clock
 from collections import defaultdict
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import pretty_print as pp
+
 
 ## local modules ##
 import percolations as perc
 
+###############################################
 ### data structures ###
 d_node_age={} # d_node_age[nodenumber] = ageclass
 d_simresults = {} # d_simresults[(T, simnumber)] = (number of infected children, number of infected adults)
-d_resincid = defaultdict(list) # d_resincid[(T, 'C' or 'A')] = [child/adult incidence simulation1, child/adult incidence simulation2,...]
-d_resOR = defaultdict(list) # d_resOR[T] = [OR sim1, OR sim2,...]
-d_resORsd = {} # d_resORsd[T] = sd of ORs across all simulations
-d_epiincid = defaultdict(list) # d_epiincid[(T, 'C' or 'A')] = [child/adult incidence epidemic1, child/adult incidence epidemic2,...]
+d_simOR = {} # d_simOR[(T, simnumber)] = OR for epidemic or non-epidemic
+d_simOR2 = defaultdict(list) # d_simOR2[T] = [OR sim1, OR sim2,...]
 d_epiOR = defaultdict(list) # d_epiOR[T] = [OR epidemic1, OR epidemic2,...]
-d_epiORsd = {} # d_epiORsd[T] = sd of ORs across simulations that resulted in epidemics
-d_episize = defaultdict(list) # epidemic sizes of epidemics only
-d_ressize = defaultdict(list) # epidemics sizes of all simulations
+d_ressize = defaultdict(list) # d_ressize[T] = [simsize1, simsize2,...]
 
+# for diagnostics
+d_episize = defaultdict(list) # d_episize[T] = [episize1, episize2, etc...]
+d_numepi = {} # d_numepi[cov] = number of epidemics produced from that coverage level
 
+###############################################
 ### parameters ###
-numsims = 2000 # number of simulations
+numsims = 4000 # number of simulations
 size_epi = 515 # threshold value that designates an epidemic in the network
-# Tlist = np.linspace(0, .2, num=21, endpoint=True) # probability of transmission
-Tlist = np.linspace(0.04, 0.07, num = 16, endpoint=True) # zoom in on realistic values
+Tlist = np.linspace(0, .2, num=21, endpoint=True) # probability of transmission
+# Tlist = np.linspace(0.04, 0.07, num = 16, endpoint=True) # zoom in on realistic values
 
-
+###############################################
 ### import data ###
 f = open('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Data/urban_edges_Sarah.csv') # Vancouver network
 file2 = open('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Data/urban_ages_Sarah.csv') # node number and age class
 
+###############################################
 ### construct age-structured network ###
 G=nx.Graph()
 for edge in f:
@@ -68,7 +71,8 @@ for line in file2:
     new_line = line.split()
     for line in new_line:
         node, age = line.split(',')
-        d_node_age[node] = age # node-ageclass dictionary
+		# node-ageclass dictionary
+        d_node_age[node] = age 
 
 net_size = float(G.order())
 print "network size:", net_size
@@ -84,129 +88,131 @@ for T in Tlist:
 	print "T value for current simulations:", T
 	# d_binlist[simnumber] = [list of 0s and 1s in node numbered index - 1 if node was infected in simnumber simulation]
 	d_binlist = defaultdict(list) 
+	
+	# simulations
 	start = clock()
-	for num in np.arange(numsims):
-		child_rec, adult_rec, total_rec, bin_list = perc.perc_age_gen(G, d_node_age, T)
+	for num in xrange(numsims):
+		child_rec, adult_rec, total_rec, bin_list, OR_val = perc.perc_age_gen(G, d_node_age, T)
 		d_simresults[(T, num)] = (child_rec, adult_rec, total_rec)
 		d_binlist[num] = bin_list
+		d_simOR[(T, num)] = OR_val
 	print "simtime, simnum:", clock()-start, "\t", num
 
 	# print binary file of infecteds for each set of T simulations
-	filename = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Results/binlist_T_%ssims_T%s_vax0.txt' %(numsims, str(T))
+	filename = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Results/binlist_T_%ssims_T%.3f_vax0.txt' %(numsims, T)
 	pp.print_dictlist_to_file(d_binlist, filename)
 
 ##############################################
-### calculate OR for all results ###
-# # calculate incidence of children and adults
-# for key in d_simresults:
-# 	d_resincid[(key[0], 'C')].append(d_simresults[key][0]/c_size)
-# 	d_resincid[(key[0], 'A')].append(d_simresults[key][1]/a_size)
-#
-# ### clean up all size results (all results) ###
-# for T in Tlist:
-# 	d_ressize[T] = [d_simresults[key][2] for key in d_simresults if T == key[0]]
-#
-# ### calculate OR of children to adults incidence for all epidemics ###
-# # calculate OR
-# for T in Tlist:
-# # 	print T, d_epiincid[(T, 'C')], d_epiincid[(T,'A')]
-# 	numerator = [(item/(1-item)) for item in d_resincid[(T, 'C')]]
-# 	denominator = [(item/(1-item)) for item in d_resincid[(T, 'A')]]
-# 	d_resOR[T] = [n/d for n, d in zip(numerator, denominator) if n != 0 and d != 0]
-# 	d_resORsd[T] = np.std(d_resOR[T]) # for errorbars in plot
-# 	d_ressize[T] = [d_simresults[key][2] for key in d_simresults if T == key[0]]
-# 	# need to remove item from d_ressize list, indexes where numerator or denominator equal 0 because OR cannot be divided by 0. 8/14/13 ECL
+### subset: epidemics only ###
 
-# NOTE: how to treat data when item == 1??
-
-##############################################
-### calculate OR for epidemics only ###
-# # separate epidemics from all results
+# subset epidemics from all results
+# key = (T, num), val = (child_rec, adult_rec, total_rec)
 d_simepi = perc.epidemicsonly(d_simresults, size_epi) 
 
-# calculate incidence of children and adults (epidemics only)
-for key in d_simepi.keys():
-	d_epiincid[(key[0], 'C')].append(d_simepi[key][0]/c_size)
-	d_epiincid[(key[0], 'A')].append(d_simepi[key][1]/a_size)
-
-### calculate OR of children to adults incidence for all epidemics ###
-# grab unique list of T values that produced at least one epidemic
-T_epi = list(set([key[0] for key in d_simepi]))
-# calculate OR
-for T in T_epi:
-# 	print T, d_epiincid[(T, 'C')], d_epiincid[(T,'A')]
-	numerator = [(item/(1-item)) for item in d_epiincid[(T, 'C')]]
-	denominator = [(item/(1-item)) for item in d_epiincid[(T, 'A')]]
-	d_epiOR[T] = [n/d for n,d in zip(numerator, denominator)]
-	d_epiORsd[T] = np.std(d_epiOR[T]) # for errorbars in plot
-
-### clean up episize (epidemics only) ###
-T_epi = list(set([key[0] for key in d_simepi]))
-for T in T_epi:
-	d_episize[T] = [d_simepi[key][2] for key in d_simepi if T == key[0]]
-d_episize_sd = dict((k, np.std(d_episize[k])) for k in d_episize)
-
-
+# subset ORs for sims that produced epidemics
+# key = T, val = [OR epi1, OR epi2,...]
+for key in sorted(d_simepi):
+	d_epiOR[key[0]].append(d_simOR[key])
 
 ##############################################
-### plot OR by T ###
-plt.errorbar(T_epi, [np.mean(d_epiOR[T]) for T in T_epi], yerr=[d_epiORsd[T] for T in T_epi], marker='o', color='black', linestyle='None')
+### clean x-axis values ###
+
+# unique T values that produced at least one epidemic
+T_epi = sorted(list(set([key[0] for key in d_simepi])))
+
+# total epidemic sizes for each T, epidemics only
+for T in T_epi:
+	# d_episize[T] = [episize1, episize2,...]
+	d_episize[T] = [d_simepi[key][2] for key in sorted(d_simepi) if T == key[0]]
+
+# separate total epidemic sizes and ORs for all results by T
+for T in Tlist:
+
+	# note: d_simresults[(T, simnumber)] = (child_rec, adult_rec, total_rec)
+	# d_ressize[T] = [simsize1, simsize2,...]
+	d_ressize[T] = [d_simresults[key][2] for key in sorted(d_simresults) if T == key[0]]
+	
+	# note: d_simOR[(T, simnumber)] = OR_val
+	# d_simOR2[T] = [OR sim1, OR sim2,...]
+	d_simOR2[T] = [d_simOR[key] for key in sorted(d_simOR) if T == key[0]]
+
+##############################################
+### RESULTS: OR by T w/ error bars ###
+
+# plot
+plt.errorbar(T_epi, [np.mean(d_epiOR[T]) for T in T_epi], yerr=[np.std(d_epiOR[T]) for T in T_epi], marker='o', color='black', linestyle='None')
 plt.xlabel('T')
 plt.ylabel('OR, child:adult')
-plt.xlim([-.05, 0.25])
-figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/epiOR_T_%ssims_T%s-%s_vax0.png' %(numsims, str(min(Tlist)), str(max(Tlist)))
+plt.xlim([0, 0.25])
+figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/epiOR_T_%ssims_T%.3f-%.3f_vax0.png' %(numsims, min(Tlist), max(Tlist))
 plt.savefig(figname)
-# plt.show()
 plt.close()
-
-# ECL 8/14/13 dictionaries need to be fixed
-# ### plot OR by epidemic size (all results) ###
-# ressize, resOR = [],[]
-# for T in sorted(Tlist):
-# 	for item in d_ressize[T]:
-# 		ressize.append(item)
-# 	for item in d_epiOR[T]:
-# 		resOR.append(item)
-# plt.scatter(ressize, resOR, marker = 'o', facecolors='none', edgecolors='black', s = 45, label='all sims')
-# plt.xlabel('epidemic size')
-# plt.ylabel('OR, child:adult')
-# plt.legend(loc = 'upper left')
-# figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/resOR_size_T_%ssims_T%s-%s_vax0.png' %(numsims, str(min(Tlist)), str(max(Tlist)))
-# # plt.savefig(figname)
 # plt.show()
-# plt.close()
 
-### plot OR by episize (epidemics only) ###
-episize, epiOR = [],[]
-for T in sorted(T_epi):
+##############################################
+### RESULTS: OR by episize (epidemics only) ###
+
+# colormap formatting
+colors = cm.rainbow(np.linspace(0, 1, len(T_epi)))
+
+# separate information from dicts for each T
+for T, col in zip(T_epi, colors):
+	episize = []
+	# d_episize[T] = [episize1, episize2,...]
 	for item in d_episize[T]:
 		episize.append(item)
-	for item in d_epiOR[T]:
-		epiOR.append(item)
-plt.scatter(episize, epiOR, marker = 'o', facecolors='none', edgecolors='blue', s = 45, label='epidemics only')
-plt.xlabel('epidemic size')
+
+	# one plot per T
+	# note: d_epiOR[T] = [OR epidemic1, OR epidemic2,...]
+	lab_dummy = 'T=' + str(T)
+	plt.scatter(episize, d_epiOR[T], marker = 'o', facecolors = 'none', edgecolors = col, s = 45, label = lab_dummy)
+plt.xlabel('epidemic size, epidemics only')
 plt.ylabel('OR, child:adult')
-plt.legend(loc = 'upper left')
-figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/epiOR_size_T_%ssims_T%s-%s_vax0.png' %(numsims, str(min(Tlist)), str(max(Tlist)))
+plt.xlim([0, 10000])
+plt.ylim([0, 10])
+plt.legend(loc = 'upper left', prop = {'size':8})
+figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/epiOR_size_T_%ssims_T%.3f_vax0.png' %(numsims, T)
 plt.savefig(figname)
-# plt.show()
 plt.close()
+# plt.show()
+	
+##############################################
+### RESULTS: OR by episize (all results) ###
+
+# colormap formatting
+colors = cm.rainbow(np.linspace(0, 1, len(Tlist)))
+
+# separate information from dicts for each T
+for T, col in zip(Tlist, colors):
+	# one plot per T value
+	lab_dummy = 'T=' + str(T)
+	plt.scatter(d_ressize[T], d_simOR2[T], marker = 'o', facecolors = 'none', edgecolors = col, s = 45, label = lab_dummy)
+plt.xlabel('epidemic size, all sims')
+plt.ylabel('OR, child:adult')
+plt.xlim([0, 10000])
+plt.ylim([0, 10])
+plt.legend(loc = 'upper center', prop = {'size':6})
+figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/simOR_size_T_%ssims_T%.3f_vax0.png' %(numsims, T)
+plt.savefig(figname)
+plt.close()
+# plt.show()
 
 ##############################################
-### DIAGNOSTICS: epidemic size w/ error bars ###
+### DIAGNOSTICS: epidemic size w/ error by T ###
+
 # plot episize by T
-plt.errorbar(T_epi, [np.mean(d_episize[T]) for T in T_epi], yerr=[d_episize_sd[T] for T in T_epi], marker='o', color='black', linestyle='None')
+plt.errorbar(T_epi, [np.mean(d_episize[T]) for T in T_epi], yerr=[np.std(d_episize[T]) for T in T_epi], marker='o', color='black', linestyle='None')
 plt.xlabel('T')
 plt.ylabel('epidemic size')
-plt.xlim([-.05, 0.25])
-figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/episize_T_%ssims_T%s-%s_vax0.png' %(numsims, str(min(Tlist)), str(max(Tlist)))
+plt.xlim([0, 0.25])
+plt.ylim([0, 10000])
+figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/episize_T_%ssims_T%.3f-%.3f_vax0.png' %(numsims, min(Tlist), max(Tlist))
 plt.savefig(figname)
-# plt.show()
 plt.close()
+# plt.show()
 
-### DIAGNOSTICS: number of epidemics ### 
-d_numepi ={}
-T_epi = list(set([key[0] for key in d_simepi]))
+##############################################
+### DIAGNOSTICS: number of epidemics by T ### 
 for T in T_epi:
 	d_numepi[T] = len([d_simepi[key][2] for key in d_simepi if T == key[0]])
 
@@ -214,15 +220,15 @@ for T in T_epi:
 plt.plot(sorted(T_epi), [d_numepi[T] for T in sorted(T_epi)], marker='o', color='black')
 plt.xlabel('T')
 plt.ylabel('number of epidemics')
-plt.xlim([-.05, 0.25])
-figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/numepi_T_%ssims_T%s-%s_vax0.png' %(numsims, str(min(Tlist)), str(max(Tlist)))
+plt.xlim([0, 0.25])
+figname = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Figures/numepi_T_%ssims_T%.3f-%.3f_vax0.png' %(numsims, min(Tlist), max(Tlist))
 plt.savefig(figname)
-# plt.show()
 plt.close()
+# plt.show()
 
 
 ##############################################
 ### write dictionaries to files ###
-filename = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Results/epiOR_T_%ssims_T%s-%s_vax0.txt'%(numsims, str(min(Tlist)), str(max(Tlist)))
+filename = '/home/elee/Dropbox/Elizabeth_Bansal_Lab/Age_Based_Simulations/Results/epiOR_T_%ssims_T%.3f-%.3f_vax0.txt' %(numsims, min(Tlist), max(Tlist))
 pp.print_OR_to_file(d_epiOR, filename)
 
